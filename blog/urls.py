a916de2caaf944e5435b7e8d0024fd62d254931b
Lucas de la Arena Coll <lucasdlarena@gmail.com>
2023-11-23 15:28:58 -0300

    Acerca de

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from .views import home, about, detalle_articulo, crear_articulo, editar_articul
 
 urlpatterns = [
     path('', home, name='home'),
-    path('about/', about, name='about'),
+    path('acerca_de/', about, name='about'),
     path('pages/<int:articulo_id>/', detalle_articulo, name='detalle_articulo'),
     path('crear_articulo/', crear_articulo, name='crear_articulo'),
     path('editar_articulo/<int:articulo_id>/', editar_articulo, name='editar_articulo'),
