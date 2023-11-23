0504cf790e8ab907211cdd897384e65edf7120a7
Lucas de la Arena Coll <lucasdlarena@gmail.com>
2023-11-23 15:20:56 -0300

    Acerca de

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, about, detalle_articulo, crear_articulo, editar_articulo, eliminar_articulo
+from .views import home, about, detalle_articulo, crear_articulo, editar_articulo, eliminar_articulo, lista_articulos
 
 urlpatterns = [
     path('', home, name='home'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('crear_articulo/', crear_articulo, name='crear_articulo'),
     path('editar_articulo/<int:articulo_id>/', editar_articulo, name='editar_articulo'),
     path('eliminar_articulo/<int:articulo_id>/', eliminar_articulo, name='eliminar_articulo'),
+    path('lista_articulos/', lista_articulos, name='lista_articulos')
 ]
