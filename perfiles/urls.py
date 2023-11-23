c0f3433e1bd534bc05484ec6681ac2ae3f1f8e73
Lucas de la Arena Coll <lucasdlarena@gmail.com>
2023-11-23 16:35:54 -0300

    Intentando solucionar path accounts

--- perfiles/urls.py
+++ perfiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import registro, inicio_sesion, perfil
+from .views import registro, inicio_sesion, perfil, editar_perfil
 
 urlpatterns = [
     path('signup/', registro, name='registro'),
     path('login/', inicio_sesion, name='inicio_sesion'),
     path('profile/', perfil, name='perfil'),
+    path('edit-profile/', editar_perfil, name='editar_perfil')
 ]
